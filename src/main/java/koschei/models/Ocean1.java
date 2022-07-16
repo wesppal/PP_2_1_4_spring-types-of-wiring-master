6b3b08d3731c36ed10fc73a590aaf684a206dd65
Pavel Ivanchik <ivanchik.pal@gmail.com>
2022-07-16 21:22:37 +0300

    C1

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -1,13 +1,19 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Ocean1 {
 
-    @Autowired
+
     private Island2 island;
+
+    @Autowired
+    public Ocean1(@Qualifier("island2") Island2 island) {
+        this.island = island;
+    }
 
     @Override
     public String toString() {
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
+
+    private Duck5 dug;
+    @Autowired
+    public Rabbit4(Duck5 dug) {
+        this.dug = dug;
+    }
+
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + dug.toString();
     }
 }
